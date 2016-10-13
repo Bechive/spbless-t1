eb6628675be15e4a2aefbad6d7d045c2d77f0947
Jeremy Heritage <jheritage@gmail.com>
2016-10-13 18:37:59 +1100

    partial implmentation and pseudo code of decrypt-by-BotMaster

--- master_view.py
+++ master_view.py
@@ -5,6 +5,25 @@ def decrypt_valuables(f):
     # TODO: For Part 2, you'll need to decrypt the contents of this file
     # The existing scheme uploads in plaintext
     # As such, we just convert it back to ASCII and print it out
+    private_key = RSA.importKey(open('masterkey.pem', 'rb').read())
+
+    # New PKCS1_OAEP Encrypt/Decrypt instance
+    pkcs1_cipher = PKCS1_OAEP.new(private_key)
+
+    #find key size + 1 in bytes (/ 8)
+
+    # get the prefix bytes/word from the file
+
+    # get the remaining bytes/word from the file
+
+    # Decrypt asymmetric ciphertext
+    # - split the data up to retrieve actual message, iv, symm key etc.
+
+    # Decrypt symmetric ciphertext
+    # - then use the above in symmetric cipher
+
+    # print(--- decrypted message ---)
+    
     decoded_text = str(f, 'ascii')
     print(decoded_text)
 
